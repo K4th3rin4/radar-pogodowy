5444df86dba375d0d04a23d613032bc70cb5d7b3
K4th3rin4 <kasiabakala91@gmail.com>
2026-06-19 08:20:17 +0200

    Bump cache v18

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategia: network-first dla wszystkiego
 // Cache tylko jako fallback gdy offline
 
-const CACHE = 'radar-v17';
+const CACHE = 'radar-v18';
 
 self.addEventListener('install', e => {
   self.skipWaiting(); // Aktywuj od razu bez czekania
